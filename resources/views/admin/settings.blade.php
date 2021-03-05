a9c780db9ed69aa05b0052166704c7ed63abd196
achaghour zakaria <achaghourzakaria@gmail.com>
2021-03-05 16:29:56 +0100

    settings still need controller update

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -24,38 +24,83 @@
                 <!-- ============================================================== -->
                 <!-- Start Page Content -->
                 <!-- ============================================================== -->
-                <div class="card">
-                    <div class="card-body ">
+
+                <div class="card row">
+                    <div class="card-body col-md-8 mx-auto">
                         
                         <h6 class="card-subtitle"></h6>
                         <form id="account-form" action="{{ route('profile_edit') }}" class="m-t-40">
                             @csrf
-                            @method('PATCH')
+                           @method('PATCH')
+                               
+                           <div class="row">
+                            <div class="col-md-6">
+                                <label for="firstname">First name *</label>
+                                <input id="firstname" name="firstname" type="text" value="{{ $user->firstname }}" autocomplete="false" class="form-control" required >
+                            </div>
+                            <div class="col-md-6">
+                                <label for="lastname">Last name *</label>
+                              <input id="lastname" name="lastname" type="text" value="{{ $user->lastname }}" autocomplete="false" class="form-control" required >
+                             </div>
+
+                        </div>
+                           <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="email">Email :</label>
+                                    <input type="email" class="form-control" name="email" id="email"
+                                    value="{{ old('email', $user->email ?? null) }}" 
+                                    placeholder="example@example.com" autocomplete="false" required>
+                         
+                                      </div>
+                                </div>
+                                </div>
+                                
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                          
+                                    <label for="email">Email *</label>
+                                    <input id="email" name="email" type="text" value="{{ $user->email }}"autocomplete="false" class="required email form-control">
+                                    
+                                        </div>
+                                    </div>
+                                </div>
                             
-                                <section>
-                                    <label for="userName">User name *</label>
-                                    <input id="userName" name="userName" type="text"value="{{ $user->name }}" class="form-control" required >
-                                    <label for="current_password">Current Password *</label>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="current_password">Current Password *</label>
                                     <span id="pwdchk"></span>
                                     <input id="current_password" name="current_password" type="password" class="required form-control">
                                     
-                                    <label for="new_password">New Password *</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="new_password">New Password *</label>
                                     <input id="new_password" name="new_password" type="password" class="required form-control">
-                                    <label for="confirm_password">Confirm Password *</label>
-                                    <input id="confirm_password" name="confirm_password" type="password" class="required form-control">
                                    
-                                </section>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="confirm_password">Confirm Password *</label>
+                                            <input id="confirm_password" name="confirm_password" type="password" class="required form-control">
+                                           
+                                        </div>
+                                    </div>
+                                </div>
+                                     
+                                   
+                              
+                                   
                                 
-                                <section>
-                                    <label for="firstname">First name *</label>
-                                    <input id="firstname" name="firstname" type="text" value="{{ $user->firstname }}" class="form-control" required >
-                                    <label for="lastname">Last name *</label>
-                                    <input id="lastname" name="lastname" type="text" value="{{ $user->lastname }}" class="form-control" required >
-                                    <label for="email">Email *</label>
-                                    <input id="email" name="email" type="text" value="{{ $user->email }}" class="required email form-control">
-                                    
-                                   
-                                </section>
                                
                         
                                 <div class="row">
@@ -72,7 +117,59 @@
                         </form>
                     </div>
                 </div>
-    
+          
+            <!--<div class="card">
+                <form id="account-form" action="{ route('profile_edit') }}" class="form-horizontal">
+
+                <form class="form-horizontal">
+                    <div class="card-body">
+                        <h4 class="card-title">Personal Info</h4>
+                        <div class="form-group row">
+                            <label for="fname" class="col-sm-3 text-right control-label col-form-label">First Name</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="fname" placeholder="First Name Here">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="lname" class="col-sm-3 text-right control-label col-form-label">Last Name</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="lname" placeholder="Last Name Here">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="lname" class="col-sm-3 text-right control-label col-form-label">Password</label>
+                            <div class="col-sm-9">
+                                <input type="password" class="form-control" id="lname" placeholder="Password Here">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="email1" class="col-sm-3 text-right control-label col-form-label">Company</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="email1" placeholder="Company Name Here">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Contact No</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="cono1" placeholder="Contact No Here">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Message</label>
+                            <div class="col-sm-9">
+                                <textarea class="form-control"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="border-top">
+                        <div class="card-body">
+                            <button type="button" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            </div>-->
+
  @endsection
 
    
--- resources/views/layouts/admin/designe.blade.php
+++ resources/views/layouts/admin/designe.blade.php
@@ -153,7 +153,8 @@
     rules: {
         userName: {
             required: true,
-            minlength: 3
+            minlength: 3,
+            disabled:true
 
         },
         current_password: {
@@ -174,7 +175,8 @@
 
         email: {
              required: true,
-             email: true
+             email: true,
+             disabled:true
          },
 
          firstname: {
@@ -187,26 +189,6 @@
             }
     }
 });
-form.children("div").steps({
-        headerTag: "h3",
-        bodyTag: "section",
-        transitionEffect: "slideLeft",
-        onStepChanging: function(event, currentIndex, newIndex) {
-            form.validate().settings.ignore = ":disabled,:hidden";
-            return form.valid();
-        },
-        onFinishing: function(event, currentIndex) {
-            form.validate().settings.ignore = ":disabled";
-            return form.valid();
-        },
-        onFinished: function(event, currentIndex) {
-                
-            $("#account-form").submit();
- // Submit the form
-       
-        }
-    });
-
        
     </script>
 
